163c094a397098725324d33c1c740058044c8a24
Michal Bilinski <michalaugustyn.bilinski@gmail.com>
2025-03-09 14:58:16 +0100

    station list simple

--- web-app/src/app/components/StationList/index.tsx
+++ web-app/src/app/components/StationList/index.tsx
@@ -1,1 +1,18 @@
-//To be implemented
+"use client";
+
+import { StationListItem } from "../../types";
+import StationCard from "../StationCard";
+
+interface StationListProps {
+  stations: StationListItem[];
+}
+
+export default function StationList({ stations }: StationListProps) {
+  return (
+    <div className="station-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {stations.map((station) => (
+        <StationCard key={station.id} station={station} />
+      ))}
+    </div>
+  );
+}
