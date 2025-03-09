0e9da75c641134e452c0097b6def455be0dc3cfd
LegoFighter <32513130+LegoFighter@users.noreply.github.com>
2025-03-09 17:42:53 +0100

    revisited e2e test (#8)

--- web-app/src/__test__/e2e/app/page.spec.tsx
+++ web-app/src/__test__/e2e/app/page.spec.tsx
@@ -6,31 +6,22 @@ test.describe("HomePage e2e", () => {
   });
 
   test("User can add categories and filter by name", async ({ page }) => {
-    await page.getByText("70s").click();
-    await page.getByText("Alternative").click();
-    await page
-      .getByRole("textbox", { name: "Search by name or genre" })
-      .click();
-    await page
-      .getByRole("textbox", { name: "Search by name or genre" })
-      .fill("101");
-    await page
-      .getByRole("link", { name: "101 WRIF 101 WRIF Alternative" })
-      .click();
-    await page.getByRole("link", { name: "Home" }).click();
+    await page.getByText('70s').click();
+    await page.getByText('90s').click();
+    await page.getByRole('textbox', { name: 'Search by name or genre' }).click();
+    await page.getByRole('textbox', { name: 'Search by name or genre' }).fill('sho');
+    await page.getByRole('link', { name: 'shonuffradio shonuffradio 80s' }).click();
+    await page.getByRole('link', { name: 'Home' }).click();
   });
 
   test("Next and previous button are not pressable when border of pages are reached.", async ({
     page,
   }) => {
-    await page.getByText("2").click();
-    await page.getByText("1", { exact: true }).click();
-    await page.getByRole("listitem").filter({ hasText: "Previous" }).click();
-    await page.getByText("2").click();
-    await page.getByLabel("Go to previous page").click();
-    await page.getByText("10", { exact: true }).click();
-    await page.getByRole("listitem").filter({ hasText: "Next" }).click();
-    await page.getByText("9", { exact: true }).click();
-    await page.getByLabel("Go to next page").click();
+    await page.getByText('2', { exact: true }).click();
+    await page.getByLabel('pagination').getByText('1').click();
+    await page.getByText('5', { exact: true }).click();
+    await page.getByRole('listitem').filter({ hasText: 'Next' }).click();
+    await page.getByRole('link', { name: 'Radio Banovina Radio Banovina' }).click();
+    await page.getByRole('link', { name: 'Home' }).click();
   });
 });
